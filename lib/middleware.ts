10e729811afd23eb67677a724f3d9058e64eac00
Dan <dan@waxworlds.org>
2025-02-27 13:56:07 +0000

    Updated logger console colour for 201 - 299 status messages

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -6,6 +6,7 @@ import config from "../config";
 
 export const faviconRequest = (req, res, next) => {
   if (req.originalUrl && req.originalUrl.split("/").pop() === "favicon.ico") {
+    console.log(chalk.yellowBright("        No content - favicon.ico"));
     return res.sendStatus(204);
   }
   next();
@@ -25,7 +26,13 @@ export const logger = (logging, logger) => (req, res, next) => {
           requestData: ${JSON.stringify(req.body)},
           userAgent: ${req.headers["user-agent"]},
         Response:
-          ${res.statusCode === 200 ? `status: ${res.statusCode}` : chalk.redBright(`status: ${res.statusCode}`)},
+          ${
+            res.statusCode === 200
+              ? `status: ${res.statusCode}`
+              : res.statusCode > 200 && res.statusCode < 300
+              ? chalk.yellowBright(`status: ${res.statusCode}`)
+              : chalk.redBright(`status: ${res.statusCode}`)
+          },
       `);
 
       end.apply(res, restArgs);
