7b36bcdf84f84efc8562153ef97918164b0d3040
Dan <dan@waxworlds.org>
2026-09-04 13:33:04 +0100

    Fixed error response

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -2,6 +2,8 @@ import "dotenv/config";
 import path from "path";
 import chalk from "chalk";
 
+import type { CustomError } from "./definitions";
+
 export const faviconRequest = (req, res, next) => {
   if (req.originalUrl && req.originalUrl.split("/").pop() === "favicon.ico") {
     console.log(chalk.yellowBright("        No content - favicon.ico"));
@@ -48,9 +50,9 @@ export const acceptedExtensions = (acceptedExtensions, restrictedEndpoints) => (
   if (restricted && extensionRejected) {
     console.error(chalk.redBright(`        Forbidden file extension: ${decodeURIComponent(req.path)}`));
     const err = new Error(`Forbidden file extension: ${decodeURIComponent(req.path)}`);
-    err.statusCode = 403;
+    (err as CustomError).statusCode = 403;
     err.stack = "";
-    next(err);
+    return next(err);
   }
   next();
 };
