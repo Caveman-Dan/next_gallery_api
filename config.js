fdb06bda8f89878b6a00cfbefc8fb931a22c8d1a
Dan <dan@waxworlds.org>
2026-09-05 14:51:33 +0100

    Set max age for browser cached images

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
 import "dotenv/config";
 
 export default {
-  port: 8983,
+  port: 8983, // Listening port
   logging: {
-    active: true,
-    excludedRoutes: ["/status"],
+    active: true, // enable/disable logging
+    excludedRoutes: ["/status"], // exclude on these routes
   },
   httpConfig: {
-    index: false,
-    acceptedExt: ["jpg", "jpeg", "png"],
-    restrictedEndpoints: [process.env.GET_IMAGE_ENDPOINT],
+    // express.static config
+    index: false, // Do not serve index.html
+    maxAge: "1d", // How long browsers / proxies may reuse the file without asking again
+    etag: true, // 304 Not Modified when the file bytes have not changed (If-None-Match)
+    lastMadified: true, // 304 when the file's mtime has not changed (If-Modified-Since)
+    acceptedExt: ["jpg", "jpeg", "png"], // Allow-list for get_image (acceptedExtensions middleware). Not a serve-static option
+    restrictedEndpoints: [process.env.GET_IMAGE_ENDPOINT], // Paths that must go through that allow-list. Not a serve-static option.
   },
   cache: {
-    folder: "image_cache",
+    folder: "image_cache", // name of folder used for image caching
     cacheVersion: 1, // bump version & restart server to refresh image cache
   },
 };
