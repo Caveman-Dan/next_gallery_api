6553e8bbd99bb24da56c2162048f1ab55bfe5ae8
Dan <dan@waxworlds.org>
2026-09-09 14:55:22 +0100

    runServer.sh was running node twice

--- runServer.sh
+++ runServer.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-export NVM_DIR=$HOME/.nvm;
-source $NVM_DIR/nvm.sh;
+export NVM_DIR="$HOME/.nvm"
+# shellcheck source=/dev/null
+source "$NVM_DIR/nvm.sh"
 
-cd /home/dan/node/next_gallery_api
+cd /home/dan/node/next_gallery_api || exit 1
 
-nvm use 22 && npm run start
-
-npm run start
+nvm use 22 && npm start
 
 #node --import=tsx next_gallery_api.ts
