577ab592057f3a5b294ba3bffc028588554f8db6
Dan <dan@waxworlds.org>
2026-09-09 14:37:29 +0100

    updated config.ts

--- config.ts
+++ config.ts
@@ -25,7 +25,7 @@ export default {
   },
   rateLimit: {
     windowMs: 60_000, // How long to remember requests for, in milliseconds.
-    trustProxy: true, // When Node sits behind a reverse proxy, use the client IP (not the proxy) for rate limits.
+    trustProxy: false, // When Node sits behind a reverse proxy, use the client IP (not the proxy) for rate limits.
     api: 300, // all /api
     getImages: 60, // Sharp metadata + blur
     transform: 120, // ?w= only; skip plain originals
